2be3ee7ed21594ede2170c63d5ab2b1d7b932042
kipngenohaaron <kipngenohaaron@gmail.com>
2023-06-11 18:46:16 +0300

    background color

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 // script.js
-const skillsList = document.getElementById('skillsList');
-
-skillsList.addEventListener('click', (event) => {
-  if (event.target.tagName === 'LI') {
-    event.target.classList.toggle('selected');
+// Change background color
+function changeBackgroundColor() {
+    const body = document.querySelector('body');
+    body.style.backgroundColor = getRandomColor();
   }
-});
+  
+  // Generate a random color
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  // Add event listener to the header for background color change
+  const header = document.querySelector('header');
+  header.addEventListener('click', changeBackgroundColor);
+  
+  // Show additional information
+  function showAdditionalInfo() {
+    const additionalInfo = document.querySelector('.additional-info');
+    additionalInfo.classList.toggle('hide');
+  }
+  
+  // Add event listener to the "Interest and Skills" section for additional information
+  const skillsHeader = document.querySelector('h2');
+  skillsHeader.addEventListener('click', showAdditionalInfo);
+  
