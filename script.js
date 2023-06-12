0567ac8bd7fdacd5054d7176dcec41d631156a30
kipngenohaaron <kipngenohaaron@gmail.com>
2023-06-12 16:09:10 +0300

    contacts page updated

--- script.js
+++ script.js
@@ -35,3 +35,24 @@ function changeBackgroundColor() {
 
 sr.reveal('.about,.services,.portfolio,.contact',{delay:200, origin:'bottom'})
 
+// Event listener for form submission
+const contactForm = document.getElementById('contactForm');
+const submitButton = document.getElementById('submitButton');
+
+contactForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  displayMessage();
+});
+
+// Function to display a message after form submission
+function displayMessage() {
+  const messageContainer = document.createElement('div');
+  messageContainer.textContent = 'Thank you for your message! We will get back to you soon.';
+  messageContainer.classList.add('message');
+  
+  const section = document.querySelector('section');
+  section.appendChild(messageContainer);
+
+  // Reset form fields after submission
+  contactForm.reset();
+}
